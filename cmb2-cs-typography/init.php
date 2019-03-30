ef2a598a0d030e0e27da51e38e7c3a32bec77491
Hisham Radi <code.spacing@gmail.com>
2019-03-30 12:50:05 +0100

    Update init.php

--- cmb2-cs-typography/init.php
+++ cmb2-cs-typography/init.php
@@ -10,9 +10,6 @@
  */
 
 function cmb2_init_cs_typography_field() {
-	
 	require_once dirname( __FILE__ ) . '/cmb2-cs-typography.php';
-
 }
-
 add_action('cmb2_init', 'cmb2_init_cs_typography_field');
